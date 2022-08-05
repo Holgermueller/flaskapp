f6b2deac72eea261b8ae4a5464b0a710076f13ae
holgermueller <9jholger@gmail.com>
2022-08-05 17:16:36 -0500

    rendered html to dom

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from crypt import methods
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1>Welcome to the app</h1>", 200
+    return render_template('index.html')
+
+
+@app.route("/message", methods=["POST"])
+def form_post():
+    if request.method == "POST":
+        name = request.form.get("name")
+    return render_template("message.html", name=name)
+
+
+@app.route("/about")
+def about():
+    return "<h1>This is a Flask web application.</h1>"
 
 
 if __name__ == "__main__":
