afbb56397a4d8822a2e38c612e7736df474de3d6
holgermueller <9jholger@gmail.com>
2022-08-06 15:29:37 -0500

    added routing

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run()
