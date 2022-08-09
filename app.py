5a0239766c174bf045d70c43e9cde0c26a777141
holgermueller <9jholger@gmail.com>
2022-08-09 18:05:38 -0500

    got data from request onto dom

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from urllib import request
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+from googleapiclient import discovery
+import json
 
 app = Flask(__name__)
+
+def config():
+    load_dotenv()
+
+config()
+
+api_key = os.getenv('api_key')
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -10,7 +21,29 @@ def hello():
 @app.route('/', methods=['POST'])
 def get_data_from_form():
     if request.method == 'POST':
-        result = request.form
+
+        client = discovery.build(
+        "commentanalyzer",
+        "v1alpha1",
+        developerKey=api_key,
+        discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+        static_discovery=False,
+        )
+
+        # analyze_request = {
+        # 'comment': { 'text': 'friendly greetings from python' },
+        # 'requestedAttributes': {'TOXICITY': {}}
+        # }
+
+        analyze_request = {
+            'comment': {'text': 'request.form'},
+            'requestedAttributes': {'TOXICITY': {}}
+        }
+
+        response = client.comments().analyze(body=analyze_request).execute()
+        print(json.dumps(response, indent=2))
+
+        result = response
         return render_template('index.html', result=result)
 
 
