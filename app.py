4d9dc03cf1e379236bb677bdff3967e31bfafefc
holgermueller <9jholger@gmail.com>
2022-08-09 17:43:15 -0500

    form sends data to server

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def get_data_from_form():
+    if request.method == 'POST':
+        result = request.form
+        return result
 
 
 
