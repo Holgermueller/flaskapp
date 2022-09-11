b1ca1db736dc896ba50892b1670939a603040cab
holgermueller <9jholger@gmail.com>
2022-09-10 21:28:23 -0500

    got data from server to dom

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def base():
-    return render_template('index.html')
+    data = 'Button clicked'
+    return render_template('index.html', data=data)
 
 
 if __name__ == "__main__":
